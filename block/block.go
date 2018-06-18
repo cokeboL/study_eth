124c980adafead5218fc261b4210850c12f5042e
pencil180430 <051@lydjy.net>
2018-06-18 17:19:32 +0800

    + block

---
+++ block/block.go
@@ -0,0 +1,161 @@
+package main
+
+import (
+	"bytes"
+	"crypto/sha256"
+	"encoding/binary"
+	"encoding/hex"
+	"fmt"
+	"github.com/lvlcainiao/framework/util"
+	"io"
+	"time"
+)
+
+const (
+	BLOCK_HEAD_SIZE = 20
+
+	BLOCK_HASH_SIZE = 64
+)
+
+var (
+	ZERO_HASH_STR = string(bytes.Repeat([]byte{'0'}, BLOCK_HASH_SIZE))
+)
+
+type Block struct {
+	Index     uint64
+	Timestamp uint64
+	DataLen   uint32
+	Data      string
+	PreHash   string
+	Hash      string
+}
+
+func (b *Block) Calculate() string {
+	str := fmt.Sprintf("%d%d%d%s%s", b.Index, b.Timestamp, b.DataLen, b.Data, b.PreHash)
+	h := sha256.New()
+	h.Write(util.StrToBytes(str))
+	hashed := h.Sum(nil)
+	return hex.EncodeToString(hashed)
+}
+
+func (b *Block) IsValid() bool {
+	return b.Hash == b.Calculate()
+}
+
+func (b *Block) WriteTo(w io.Writer) {
+	binary.Write(w, binary.LittleEndian, &b.Index)
+	binary.Write(w, binary.LittleEndian, &b.Timestamp)
+	binary.Write(w, binary.LittleEndian, &b.DataLen)
+	binary.Write(w, binary.LittleEndian, util.StrToBytes(b.Data))
+	binary.Write(w, binary.LittleEndian, util.StrToBytes(b.PreHash))
+	binary.Write(w, binary.LittleEndian, util.StrToBytes(b.Hash))
+}
+
+func (b *Block) ReadFrom(r io.Reader) {
+	binary.Read(r, binary.LittleEndian, &b.Index)
+	binary.Read(r, binary.LittleEndian, &b.Timestamp)
+	binary.Read(r, binary.LittleEndian, &b.DataLen)
+
+	data := make([]byte, b.DataLen)
+	binary.Read(r, binary.LittleEndian, data)
+	b.Data = string(data)
+
+	hash := make([]byte, BLOCK_HASH_SIZE)
+	binary.Read(r, binary.LittleEndian, hash)
+	b.PreHash = string(hash)
+
+	binary.Read(r, binary.LittleEndian, hash)
+	b.Hash = string(hash)
+}
+
+// func (b *Block) Equal(other *Block) bool {
+// 	return b.Index == other.Index && b.Timestamp == other.Timestamp && b.DataLen == other.DataLen &&
+// 		b.Data == other.Data && b.PreHash == other.PreHash && b.Hash == other.Hash
+// }
+
+func NewBlock(old *Block, data string) *Block {
+	b := &Block{
+		Timestamp: uint64(time.Now().UnixNano()),
+		DataLen:   uint32(len(data)),
+		Data:      data,
+	}
+
+	if old != nil {
+		b.Index = old.Index + 1
+		b.PreHash = old.Hash
+	} else {
+		b.PreHash = ZERO_HASH_STR
+	}
+
+	b.Hash = b.Calculate()
+
+	return b
+}
+
+func main() {
+	b1 := NewBlock(nil, "block 1")
+	if !b1.IsValid() {
+		fmt.Println("invalid block")
+		return
+	}
+
+	b2 := NewBlock(b1, "block 2")
+
+	buf := bytes.NewBuffer(nil)
+
+	b3 := NewBlock(b2, "block 3")
+
+	b := &Block{}
+
+	fmt.Println("-----------------------------------------------------------")
+	fmt.Println("b1.Index: ", b1.Index)
+	fmt.Println("b1.Timestamp: ", b1.Timestamp)
+	fmt.Println("b1.DataLen: ", b1.DataLen)
+	fmt.Println("b1.Data: ", b1.Data)
+	fmt.Println("b1.PreHash: ", b1.PreHash)
+	fmt.Println("b1.Hash: ", b1.Hash)
+	fmt.Println("-----------------------------------------------------------")
+	fmt.Println("b2.Index: ", b2.Index)
+	fmt.Println("b2.Timestamp: ", b2.Timestamp)
+	fmt.Println("b2.DataLen: ", b2.DataLen)
+	fmt.Println("b2.Data: ", b2.Data)
+	fmt.Println("b2.PreHash: ", b2.PreHash)
+	fmt.Println("b2.Hash: ", b2.Hash)
+	fmt.Println("-----------------------------------------------------------")
+	fmt.Println("b3.Index: ", b3.Index)
+	fmt.Println("b3.Timestamp: ", b3.Timestamp)
+	fmt.Println("b3.DataLen: ", b3.DataLen)
+	fmt.Println("b3.Data: ", b3.Data)
+	fmt.Println("b3.PreHash: ", b3.PreHash)
+	fmt.Println("b3.Hash: ", b3.Hash)
+	fmt.Println("-----------------------------------------------------------")
+
+	fmt.Println("-----------------------------------------------------------")
+	b1.WriteTo(buf)
+	b.ReadFrom(buf)
+	fmt.Println("-- b1.Index: ", b.Index)
+	fmt.Println("-- b1.Timestamp: ", b.Timestamp)
+	fmt.Println("-- b1.DataLen: ", b.DataLen)
+	fmt.Println("-- b1.Data: ", b.Data)
+	fmt.Println("-- b1.PreHash: ", b.PreHash)
+	fmt.Println("-- b1.Hash: ", b.Hash)
+	fmt.Println("-----------------------------------------------------------")
+	b2.WriteTo(buf)
+	b.ReadFrom(buf)
+	fmt.Println("-- b2.Index: ", b.Index)
+	fmt.Println("-- b2.Timestamp: ", b.Timestamp)
+	fmt.Println("-- b2.DataLen: ", b.DataLen)
+	fmt.Println("-- b2.Data: ", b.Data)
+	fmt.Println("-- b2.PreHash: ", b.PreHash)
+	fmt.Println("-- b2.Hash: ", b.Hash)
+	fmt.Println("-----------------------------------------------------------")
+	b3.WriteTo(buf)
+	b.ReadFrom(buf)
+	fmt.Println("-- b3.Index: ", b.Index)
+	fmt.Println("-- b3.Timestamp: ", b.Timestamp)
+	fmt.Println("-- b3.DataLen: ", b.DataLen)
+	fmt.Println("-- b3.Data: ", b.Data)
+	fmt.Println("-- b3.PreHash: ", b.PreHash)
+	fmt.Println("-- b3.Hash: ", b.Hash)
+	fmt.Println("-----------------------------------------------------------")
+}
---
+++ block/block_test.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestBlock(t *testing.T) {
+	b1 := NewBlock(nil, "block 1")
+	if !b1.IsValid() {
+		t.Fatal("invalid block")
+		return
+	}
+
+	b2 := NewBlock(b1, "block 2")
+
+	b3 := NewBlock(b2, "block 3")
+
+	b := &Block{}
+
+	t.Log("-----------------------------------------------------------")
+	t.Log("b1.Index: ", b1.Index)
+	t.Log("b1.Timestamp: ", b1.Timestamp)
+	t.Log("b1.DataLen: ", b1.DataLen)
+	t.Log("b1.Data: ", b1.Data)
+	t.Log("b1.PreHash: ", b1.PreHash)
+	t.Log("b1.Hash: ", b1.Hash)
+	t.Log("-----------------------------------------------------------")
+	t.Log("b2.Index: ", b2.Index)
+	t.Log("b2.Timestamp: ", b2.Timestamp)
+	t.Log("b2.DataLen: ", b2.DataLen)
+	t.Log("b2.Data: ", b2.Data)
+	t.Log("b2.PreHash: ", b2.PreHash)
+	t.Log("b2.Hash: ", b2.Hash)
+	t.Log("-----------------------------------------------------------")
+	t.Log("b3.Index: ", b3.Index)
+	t.Log("b3.Timestamp: ", b3.Timestamp)
+	t.Log("b3.DataLen: ", b3.DataLen)
+	t.Log("b3.Data: ", b3.Data)
+	t.Log("b3.PreHash: ", b3.PreHash)
+	t.Log("b3.Hash: ", b3.Hash)
+	t.Log("-----------------------------------------------------------")
+
+	buf := bytes.NewBuffer(nil)
+	t.Log("-----------------------------------------------------------")
+	b1.WriteTo(buf)
+	b.ReadFrom(buf)
+	t.Log("-- b1.Index: ", b.Index)
+	t.Log("-- b1.Timestamp: ", b.Timestamp)
+	t.Log("-- b1.DataLen: ", b.DataLen)
+	t.Log("-- b1.Data: ", b.Data)
+	t.Log("-- b1.PreHash: ", b.PreHash)
+	t.Log("-- b1.Hash: ", b.Hash)
+	t.Log("-----------------------------------------------------------")
+	b2.WriteTo(buf)
+	b.ReadFrom(buf)
+	t.Log("-- b2.Index: ", b.Index)
+	t.Log("-- b2.Timestamp: ", b.Timestamp)
+	t.Log("-- b2.DataLen: ", b.DataLen)
+	t.Log("-- b2.Data: ", b.Data)
+	t.Log("-- b2.PreHash: ", b.PreHash)
+	t.Log("-- b2.Hash: ", b.Hash)
+	t.Log("-----------------------------------------------------------")
+	b3.WriteTo(buf)
+	b.ReadFrom(buf)
+	t.Log("-- b3.Index: ", b.Index)
+	t.Log("-- b3.Timestamp: ", b.Timestamp)
+	t.Log("-- b3.DataLen: ", b.DataLen)
+	t.Log("-- b3.Data: ", b.Data)
+	t.Log("-- b3.PreHash: ", b.PreHash)
+	t.Log("-- b3.Hash: ", b.Hash)
+	t.Log("-----------------------------------------------------------")
+	t.Log("-- equal: ", *b == *b1, *b == *b2, *b == *b3)
+}
